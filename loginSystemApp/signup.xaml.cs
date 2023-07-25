b19637f673a49c9b4048ce7fbee5b4613e763c2d
NajdAwadh <109174559+NajdAwadh@users.noreply.github.com>
2023-07-25 10:18:07 +0300

    Add files via upload

---
+++ loginSystemApp/signup.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace loginSystemApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class signup : ContentPage
+	{
+		public signup ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
